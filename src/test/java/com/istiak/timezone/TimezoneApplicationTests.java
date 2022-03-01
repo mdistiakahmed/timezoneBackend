e634a8dcb6be3b336eb193c6d3ecc1ba36c6b354
Md Istiak Ahmed <istinishat@gmail.com>
2022-03-02 01:01:38 +0600

    Created spring boot application with security and sql dependency

---
+++ src/test/java/com/istiak/timezone/TimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istiak.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
